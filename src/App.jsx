e1b448469ac06db056363bf182c7dba49f126264
denial-E <shempeter221992@gmail.com>
2024-08-22 02:04:17 +0530

    run bro

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { productStore } from './features/Example2Redux/ProductStore';
 import Card from './components/Example2Reduxop/Card';
-// import { OperationStore } from './features/OperationStore';
-// import DisplayReduxState from './components/DisplayReduxState';
-// import UpdateReduxState from './components/UpdateReduxState';
+import { OperationStore } from './features/OperationStore';
+import DisplayReduxState from './components/DisplayReduxState';
+import UpdateReduxState from './components/UpdateReduxState';
 
 const App = () => {
   return (
     <div>
       <h1>App Comp</h1>
-      {/* <Provider store={OperationStore}>
+      <Provider store={OperationStore}>
         <DisplayReduxState />
         <UpdateReduxState />
 
-      </Provider> */}
+      </Provider>
 
       {/* eg2: */}
       <Provider store={productStore}>
